fdc60d9010c4ecbe122bf43fafe354d8441b2cac
NoaTAU <144602977+NoaTAU@users.noreply.github.com>
2025-05-28 15:52:27 +0300

    RatingExperiment abstract class, MainExperiment - main runner
    - added RatingExperiment abstract class
    - ImageRatingExperiment now derives from this class
    - added ModelRatingExperiment derives from this class
    - added a MainExperiment script to run all experiment flow,
    - added MainExperimentRunner game object to hold MainExperiment and different rating scripts

---
+++ Assets/Real - Unreal/Scripts/MainExperiment.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+
+public class MainExperiment : MonoBehaviour
+{
+    private ImageRatingExperiment imagerRatingExperiment;
+    private ModelRatingExperiment modelRatingExperiment;
+
+
+    private void Start()
+    {
+        InitExperiments();
+
+        StartCoroutine(RunAllExperiments());
+    }
+
+    private IEnumerator RunAllExperiments()
+    {
+        Debug.Log("Starting model rating experiment...");
+        TXRDataManager.Instance.LogLineToFile("Starting model rating experiment...");
+        yield return modelRatingExperiment.ShowImageSequence();
+
+        Debug.Log("Starting image rating experiment...");
+        TXRDataManager.Instance.LogLineToFile("Starting image rating experiment...");
+        yield return imagerRatingExperiment.ShowImageSequence();
+
+        Debug.Log("All experiments finished.");
+        TXRDataManager.Instance.LogLineToFile("All experiments finished.");
+    }
+
+    private void InitExperiments()
+    {
+        imagerRatingExperiment = GetComponent<ImageRatingExperiment>();
+        modelRatingExperiment = GetComponent<ModelRatingExperiment>();
+    }
+
+}
